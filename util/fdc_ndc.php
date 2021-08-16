76349a7eedbadf082f524b01d5ef853f7881c679
brian3t <ngxtri@gmail.com>
2021-08-15 21:35:55 -0700

    Import fdc_ndc.php done.

--- util/fdc_ndc.php
+++ util/fdc_ndc.php
@@ -6,7 +6,9 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Dibi\Connection;
 
-const DEBUG = true;
+const DEBUG = false;
+//const DEBUG = true;
+
 $yii_conf = require_once(dirname(__DIR__) . '/f/protected/config/override.php');
 $yii_conf = $yii_conf['components']['db'];
 $conn_str = $yii_conf['connectionString'];//mysql:host=localhost;dbname=formulary
@@ -24,7 +26,8 @@ try {
 }
 
 
-$inp = fopen(__DIR__ . '/inp/product_sample.txt', 'r');
+$inp = fopen(__DIR__ . '/inp/product.txt', 'r');
+$populate_msg = '';
 if (! $inp) die('File not found');
 $header = fgets($inp);
 if (! $header) die("Cannot open file");
@@ -40,7 +43,7 @@ try {
     while ($row = fgets($inp)) {
 //    if (DEBUG) var_dump($row);
         $vals = explode("\t", $row);
-        if (DEBUG) var_dump($vals);
+//        if (DEBUG) var_dump($vals);
         if (sizeof($vals) !== 20) {
             echo 'Number of values not 20, exiting..';
             break;
@@ -66,19 +69,36 @@ try {
         $ndc_exclude_flag = $vals[18];
         $listing_record_certified_through = $vals[19];
 
-        $new_ndc = compact('productid');
+        $new_ndc = [];
+        $csv_list_of_var_names = 'productid,productndc,producttypename,proprietaryname,proprietarynamesuffix,nonproprietaryname,dosageformname,routename
+        ,startmarketingdate,endmarketingdate,marketingcategoryname,applicationnumber,labelername,substancename,active_numerator_strength,active_ingred_unit
+        ,pharm_classes,deaschedule,ndc_exclude_flag,listing_record_certified_through';
+        $csv_list_of_var_names = str_replace(' ', '', $csv_list_of_var_names);
+        $csv_list_of_var_names = str_replace("\n", '', $csv_list_of_var_names);
+        $csv_list_of_var_names = explode(',', $csv_list_of_var_names);
+        foreach ($csv_list_of_var_names as $var_name) {
+            $value = $$var_name;
+            $new_ndc[$var_name] = $value;
+        }
+
 
         $exist_ndc = $db->query("SELECT id FROM fda_ndc WHERE productid = %s", $productid);
         $exist_ndc = $exist_ndc->fetchSingle();
-        if (empty($exist_ndc)){
-            $db->insert('fda_ndc', $new_ndc);
-        }
-        var_dump($exist_ndc);
+        if (! empty($exist_ndc)) continue;
 
+        $insert_res = $db->query('INSERT INTO fda_ndc', $new_ndc);
+        if ($insert_res instanceof Dibi\Result) {
+            if ($insert_res->count() !== 1) {
+                $populate_msg .= ". Warning: ndc not saved";
+            }
+            $ndc_id = $db->getInsertId() ?? null;
+        }
+        if (DEBUG) var_dump($insert_res);
     }
 } catch (\Dibi\Exception $e) {
-    echo "Error: " . $e->getMessage();
+    echo "Error: " . $e->getMessage(). " New NDC: ", json_encode($new_ndc);
 }
 
 fclose($inp);
+echo $populate_msg;
 
